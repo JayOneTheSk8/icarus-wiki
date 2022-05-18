2cd1cd665316904c0945b1f1fc5fd9e6f013bdf7
JayOneTheSk8 <misterjustincox@gmail.com>
2022-05-18 07:55:37 -0400

    bundle production javascript

---
+++ dist/bundle.js
@@ -0,0 +1,127 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/WikiData/characters/RusselKing.js":
+/*!***********************************************!*\
+  !*** ./src/WikiData/characters/RusselKing.js ***!
+  \***********************************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\n\nvar constants_1 = __webpack_require__(/*! ../../constants */ \"./src/constants.js\");\n\nvar headline = {\n  sectionName: constants_1.HEADLINE,\n  sectionText: 'Russel King is one of the main characters in the story \"Icarus\".'\n};\nvar appearance = {\n  sectionName: constants_1.APPEARANCE,\n  sectionText: 'Russel has an afro.'\n};\nvar abilities = {\n  sectionName: constants_1.ABILITIES,\n  sectionText: 'Russel has wings.'\n};\nexports[\"default\"] = {\n  name: \"Russel King\",\n  sections: [headline, appearance, abilities]\n};\n\n//# sourceURL=webpack://icarus-wiki/./src/WikiData/characters/RusselKing.js?");
+
+/***/ }),
+
+/***/ "./src/WikiData/characters/index.js":
+/*!******************************************!*\
+  !*** ./src/WikiData/characters/index.js ***!
+  \******************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+eval("\n\nvar __importDefault = this && this.__importDefault || function (mod) {\n  return mod && mod.__esModule ? mod : {\n    \"default\": mod\n  };\n};\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\n\nvar RusselKing_1 = __importDefault(__webpack_require__(/*! ./RusselKing */ \"./src/WikiData/characters/RusselKing.js\"));\n\nexports[\"default\"] = [RusselKing_1[\"default\"]];\n\n//# sourceURL=webpack://icarus-wiki/./src/WikiData/characters/index.js?");
+
+/***/ }),
+
+/***/ "./src/WikiData/index.js":
+/*!*******************************!*\
+  !*** ./src/WikiData/index.js ***!
+  \*******************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+eval("\n\nvar __importDefault = this && this.__importDefault || function (mod) {\n  return mod && mod.__esModule ? mod : {\n    \"default\": mod\n  };\n};\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\n\nvar characters_1 = __importDefault(__webpack_require__(/*! ./characters */ \"./src/WikiData/characters/index.js\"));\n\nvar notes_1 = __importDefault(__webpack_require__(/*! ./notes */ \"./src/WikiData/notes/index.js\"));\n\nexports[\"default\"] = {\n  characters: characters_1[\"default\"],\n  notes: notes_1[\"default\"]\n};\n\n//# sourceURL=webpack://icarus-wiki/./src/WikiData/index.js?");
+
+/***/ }),
+
+/***/ "./src/WikiData/notes/StoneFaced.js":
+/*!******************************************!*\
+  !*** ./src/WikiData/notes/StoneFaced.js ***!
+  \******************************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\n\nvar constants_1 = __webpack_require__(/*! ../../constants */ \"./src/constants.js\");\n\nvar description = {\n  sectionName: constants_1.DESCRIPTION,\n  sectionText: 'Stone-face(d) is just a common way of describing the -_- face.'\n};\nexports[\"default\"] = {\n  name: \"Stone-Faced\",\n  sections: [description]\n};\n\n//# sourceURL=webpack://icarus-wiki/./src/WikiData/notes/StoneFaced.js?");
+
+/***/ }),
+
+/***/ "./src/WikiData/notes/TrialOfSteel.js":
+/*!********************************************!*\
+  !*** ./src/WikiData/notes/TrialOfSteel.js ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\n\nvar constants_1 = __webpack_require__(/*! ../../constants */ \"./src/constants.js\");\n\nvar description = {\n  sectionName: constants_1.DESCRIPTION,\n  sectionText: 'The Trial of Steel is an event run by Radio in his mansion.'\n};\nexports[\"default\"] = {\n  name: \"Trial of Steel\",\n  sections: [description]\n};\n\n//# sourceURL=webpack://icarus-wiki/./src/WikiData/notes/TrialOfSteel.js?");
+
+/***/ }),
+
+/***/ "./src/WikiData/notes/index.js":
+/*!*************************************!*\
+  !*** ./src/WikiData/notes/index.js ***!
+  \*************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+eval("\n\nvar __importDefault = this && this.__importDefault || function (mod) {\n  return mod && mod.__esModule ? mod : {\n    \"default\": mod\n  };\n};\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\n\nvar StoneFaced_1 = __importDefault(__webpack_require__(/*! ./StoneFaced */ \"./src/WikiData/notes/StoneFaced.js\"));\n\nvar TrialOfSteel_1 = __importDefault(__webpack_require__(/*! ./TrialOfSteel */ \"./src/WikiData/notes/TrialOfSteel.js\"));\n\nexports[\"default\"] = [StoneFaced_1[\"default\"], TrialOfSteel_1[\"default\"]];\n\n//# sourceURL=webpack://icarus-wiki/./src/WikiData/notes/index.js?");
+
+/***/ }),
+
+/***/ "./src/constants.js":
+/*!**************************!*\
+  !*** ./src/constants.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\nexports.DESCRIPTION = exports.HISTORY = exports.HEADLINE = exports.APPEARANCE = exports.ABILITIES = void 0;\n/* Character Section Names */\n\nexports.ABILITIES = 'Abilities';\nexports.APPEARANCE = 'Appearance';\nexports.HEADLINE = 'Headline';\nexports.HISTORY = 'History';\n/* Note Section Names */\n\nexports.DESCRIPTION = 'Description';\n\n//# sourceURL=webpack://icarus-wiki/./src/constants.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+eval("\n\nvar __importDefault = this && this.__importDefault || function (mod) {\n  return mod && mod.__esModule ? mod : {\n    \"default\": mod\n  };\n};\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\n\nvar WikiData_1 = __importDefault(__webpack_require__(/*! ./WikiData */ \"./src/WikiData/index.js\"));\n\nconsole.log(WikiData_1[\"default\"]);\n\n//# sourceURL=webpack://icarus-wiki/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;

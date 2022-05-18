eae135eb982ea08093e309716ba86f7f5a0c09f3
JayOneTheSk8 <misterjustincox@gmail.com>
2022-05-18 17:17:54 -0400

    create Image interface

--- src-ts/DataTypes.ts
+++ src-ts/DataTypes.ts
@@ -1,17 +1,23 @@
+export interface Image {
+    url: string
+    caption?: string
+}
+
 export interface SubSection {
     subSectionTitle: string
+    subSectionImage: Image
     subSectionText: string
 }
 
 export interface PageSection {
     title: string
-    imageUrls?: Array<string>
+    imageUrls?: Array<Image>
     body: string | Array<SubSection>
 }
 
 export interface Page {
     name: string
-    imageUrls?: Array<string>
+    imageUrls?: Array<Image>
     sections: Array<PageSection>
 }
 

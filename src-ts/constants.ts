a888c45d33868c078e6e4fe1687e8cb805e7bf2f
JayOneTheSk8 <misterjustincox@gmail.com>
2022-05-18 07:55:07 -0400

    create constants

---
+++ src-ts/constants.ts
@@ -0,0 +1,8 @@
+/* Character Section Names */
+export const ABILITIES: string = 'Abilities'
+export const APPEARANCE: string = 'Appearance'
+export const HEADLINE: string = 'Headline'
+export const HISTORY: string = 'History'
+
+/* Note Section Names */
+export const DESCRIPTION: string = 'Description'
---
+++ src/constants.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DESCRIPTION = exports.HISTORY = exports.HEADLINE = exports.APPEARANCE = exports.ABILITIES = void 0;
+/* Character Section Names */
+exports.ABILITIES = 'Abilities';
+exports.APPEARANCE = 'Appearance';
+exports.HEADLINE = 'Headline';
+exports.HISTORY = 'History';
+/* Note Section Names */
+exports.DESCRIPTION = 'Description';

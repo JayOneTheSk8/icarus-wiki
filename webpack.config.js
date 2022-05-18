c0e165c8dbb0084a696e251f4272d29b0d4d49e7
JayOneTheSk8 <misterjustincox@gmail.com>
2022-05-18 07:52:28 -0400

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    watch: true,
+    entry: {
+        // Main javascript entry point
+        bundle: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        // Name same as 'entry' key
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                // In any js file
+                test: /\.js$/,
+                // Excluding node_modules
+                exclude: /node_modules/,
+                // Use the babel loader
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};

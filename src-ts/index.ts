aa5cb953e07cedc83f6b7591c649f57b450c30a5
JayOneTheSk8 <misterjustincox@gmail.com>
2022-05-19 13:39:05 -0400

    switch to mobile on window resize

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -7,21 +7,74 @@ const {
 } = WikiData
 
 /* Constants */
+const MOBILE_WIDTH_LIMIT: number = 1000
+
 const SECTION_TITLE: string = 'section-title'
 const PAGE_OPTION: string = 'page-option'
 
+const MAIN_CLASS: string = 'main'
+const PAGE_CONTENT_CLASS: string = 'page-content'
+const SECTION_SELECTOR_CLASS: string = 'section-selector'
+const PAGE_SELECTOR_CLASS: string = 'page-selector'
+const HOME_ICON_CLASS: string = 'home-icon'
+const CHARACTER_ICON_CLASS: string = 'character-icon'
+const NOTES_ICON_CLASS: string = 'notes-icon'
+
+const MAIN_MOBILE_CLASS: string = 'main-mobile'
+const PAGE_CONTENT_MOBILE_CLASS: string = 'page-content-mobile'
+const SECTION_SELECTOR_MOBILE_CLASS: string = 'section-selector-mobile'
+const PAGE_SELECTOR_MOBILE_CLASS: string = 'page-selector-mobile'
+const HOME_ICON_MOBILE_CLASS: string = 'home-icon-mobile'
+const CHARACTER_ICON_MOBILE_CLASS: string = 'character-icon-mobile'
+const NOTES_ICON_MOBILE_CLASS: string = 'notes-icon-mobile'
+
+/* State Variables */
+var usingMobile: boolean = false
+
+/* Get HTML Elements Functions */
 const createDiv = (): HTMLElement => document.createElement('div')
+const findElement = (id: string): HTMLElement => document.getElementById(id) as HTMLElement
 
 /* DOM Elements */
-const pageSelector = document.getElementById('page-selector') as HTMLElement
-const homeIcon = document.getElementById('home-icon') as HTMLElement
-const characterIcon = document.getElementById('character-icon') as HTMLElement
-const notesIcon = document.getElementById('notes-icon') as HTMLElement
+const mainContent = findElement(MAIN_CLASS)
+const pageContent = findElement(PAGE_CONTENT_CLASS)
+// Selectors
+const sectionSelector = findElement(SECTION_SELECTOR_CLASS)
+const pageSelector = findElement(PAGE_SELECTOR_CLASS)
+// Icons
+const homeIcon = findElement(HOME_ICON_CLASS)
+const characterIcon = findElement(CHARACTER_ICON_CLASS)
+const notesIcon = findElement(NOTES_ICON_CLASS)
+
+/* Change to Mobile */
+const changeToMobile = (setMobile: boolean) => {
+    mainContent.className = setMobile ? MAIN_MOBILE_CLASS : MAIN_CLASS
+    pageContent.className = setMobile ? PAGE_CONTENT_MOBILE_CLASS : PAGE_CONTENT_CLASS
+    sectionSelector.className = setMobile ? SECTION_SELECTOR_MOBILE_CLASS : SECTION_SELECTOR_CLASS
+    pageSelector.className = setMobile ? PAGE_SELECTOR_MOBILE_CLASS : PAGE_SELECTOR_CLASS
+    homeIcon.className = setMobile ? HOME_ICON_MOBILE_CLASS : HOME_ICON_CLASS
+    characterIcon.className = setMobile ? CHARACTER_ICON_MOBILE_CLASS : CHARACTER_ICON_CLASS
+    notesIcon.className = setMobile ? NOTES_ICON_MOBILE_CLASS : NOTES_ICON_CLASS
+    usingMobile = setMobile
+}
 
 /* Set Page Selectors */
 const setHomeSelectors = (): void => pageSelector.replaceChildren(...homePageSelector)
 const setCharactersSelectors = (): void => pageSelector.replaceChildren(...charactersSelector)
 const setNotesSelectors = (): void => pageSelector.replaceChildren(...notesSelector)
+
+/* Event Functions */
+const handleResize = (): void => {
+    // If the width is below the limit and we aren't using mobile elements
+    if (window.innerWidth < MOBILE_WIDTH_LIMIT && !usingMobile) {
+        // Switch to mobile view
+        changeToMobile(true)
+    } else if (window.innerWidth >= MOBILE_WIDTH_LIMIT && usingMobile) {
+        // Otherwise if width is above the limit and we are using mobile
+        // Turn off mobile view
+        changeToMobile(false)
+    }
+}
 
 /* Home Page Selectors */
 const homePageSelector: Array<HTMLElement> = []
@@ -75,3 +128,9 @@ notesIcon.onclick = setNotesSelectors
 
 /* Set Home Page by default */
 pageSelector.replaceChildren(...homePageSelector)
+
+/* Add Event Listener for resizing */
+window.addEventListener('resize', handleResize)
+
+/* Check if mobile and set */
+if (window.innerWidth < MOBILE_WIDTH_LIMIT) { changeToMobile(true) }
--- src/index.js
+++ src/index.js
@@ -6,18 +6,66 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const WikiData_1 = __importDefault(require("./WikiData"));
 const { homePage, characters, notes, } = WikiData_1.default;
 /* Constants */
+const MOBILE_WIDTH_LIMIT = 1000;
 const SECTION_TITLE = 'section-title';
 const PAGE_OPTION = 'page-option';
+const MAIN_CLASS = 'main';
+const PAGE_CONTENT_CLASS = 'page-content';
+const SECTION_SELECTOR_CLASS = 'section-selector';
+const PAGE_SELECTOR_CLASS = 'page-selector';
+const HOME_ICON_CLASS = 'home-icon';
+const CHARACTER_ICON_CLASS = 'character-icon';
+const NOTES_ICON_CLASS = 'notes-icon';
+const MAIN_MOBILE_CLASS = 'main-mobile';
+const PAGE_CONTENT_MOBILE_CLASS = 'page-content-mobile';
+const SECTION_SELECTOR_MOBILE_CLASS = 'section-selector-mobile';
+const PAGE_SELECTOR_MOBILE_CLASS = 'page-selector-mobile';
+const HOME_ICON_MOBILE_CLASS = 'home-icon-mobile';
+const CHARACTER_ICON_MOBILE_CLASS = 'character-icon-mobile';
+const NOTES_ICON_MOBILE_CLASS = 'notes-icon-mobile';
+/* State Variables */
+var usingMobile = false;
+/* Get HTML Elements Functions */
 const createDiv = () => document.createElement('div');
+const findElement = (id) => document.getElementById(id);
 /* DOM Elements */
-const pageSelector = document.getElementById('page-selector');
-const homeIcon = document.getElementById('home-icon');
-const characterIcon = document.getElementById('character-icon');
-const notesIcon = document.getElementById('notes-icon');
+const mainContent = findElement(MAIN_CLASS);
+const pageContent = findElement(PAGE_CONTENT_CLASS);
+// Selectors
+const sectionSelector = findElement(SECTION_SELECTOR_CLASS);
+const pageSelector = findElement(PAGE_SELECTOR_CLASS);
+// Icons
+const homeIcon = findElement(HOME_ICON_CLASS);
+const characterIcon = findElement(CHARACTER_ICON_CLASS);
+const notesIcon = findElement(NOTES_ICON_CLASS);
+/* Change to Mobile */
+const changeToMobile = (setMobile) => {
+    mainContent.className = setMobile ? MAIN_MOBILE_CLASS : MAIN_CLASS;
+    pageContent.className = setMobile ? PAGE_CONTENT_MOBILE_CLASS : PAGE_CONTENT_CLASS;
+    sectionSelector.className = setMobile ? SECTION_SELECTOR_MOBILE_CLASS : SECTION_SELECTOR_CLASS;
+    pageSelector.className = setMobile ? PAGE_SELECTOR_MOBILE_CLASS : PAGE_SELECTOR_CLASS;
+    homeIcon.className = setMobile ? HOME_ICON_MOBILE_CLASS : HOME_ICON_CLASS;
+    characterIcon.className = setMobile ? CHARACTER_ICON_MOBILE_CLASS : CHARACTER_ICON_CLASS;
+    notesIcon.className = setMobile ? NOTES_ICON_MOBILE_CLASS : NOTES_ICON_CLASS;
+    usingMobile = setMobile;
+};
 /* Set Page Selectors */
 const setHomeSelectors = () => pageSelector.replaceChildren(...homePageSelector);
 const setCharactersSelectors = () => pageSelector.replaceChildren(...charactersSelector);
 const setNotesSelectors = () => pageSelector.replaceChildren(...notesSelector);
+/* Event Functions */
+const handleResize = () => {
+    // If the width is below the limit and we aren't using mobile elements
+    if (window.innerWidth < MOBILE_WIDTH_LIMIT && !usingMobile) {
+        // Switch to mobile view
+        changeToMobile(true);
+    }
+    else if (window.innerWidth >= MOBILE_WIDTH_LIMIT && usingMobile) {
+        // Otherwise if width is above the limit and we are using mobile
+        // Turn off mobile view
+        changeToMobile(false);
+    }
+};
 /* Home Page Selectors */
 const homePageSelector = [];
 const homePageTitle = createDiv();
@@ -60,3 +108,9 @@ characterIcon.onclick = setCharactersSelectors;
 notesIcon.onclick = setNotesSelectors;
 /* Set Home Page by default */
 pageSelector.replaceChildren(...homePageSelector);
+/* Add Event Listener for resizing */
+window.addEventListener('resize', handleResize);
+/* Check if mobile and set */
+if (window.innerWidth < MOBILE_WIDTH_LIMIT) {
+    changeToMobile(true);
+}

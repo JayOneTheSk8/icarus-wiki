31783ed9143eccd9a6f4ebdca3cfec1409f9d60c
JayOneTheSk8 <misterjustincox@gmail.com>
2022-05-20 08:40:25 -0400

    create RusselKing page

--- src-ts/WikiData/characters/RusselKing.ts
+++ src-ts/WikiData/characters/RusselKing.ts
@@ -1,25 +1,86 @@
-import { Page, PageSection } from '../../DataTypes'
-import { ABILITIES, HEADLINE, APPEARANCE } from '../../constants'
+import { AttributesSection, GallerySection, Page, PageSection } from '../../DataTypes'
+import { HEADLINE, APPEARANCE, ATTRIBUTES, DRIVE_THUMBNAIL_PREFIX, GALLERY } from '../../constants'
 
 const headline: PageSection = {
     title: HEADLINE,
-    body: 'Russel King is one of the main characters in the story "Icarus".'
+    body: 'Russel King is one the primary protagonists of the story "Icarus". At 13, after an accident with a mythical object, he and his brother Reggie are given elemental powers. He is quite observant and informed on many ancient histories given his father Hudson King was a well renowned archaeologist. After being given special abilities, Russel seeks normalcy in a world not ready to offer it.'
+}
+
+const attribues: AttributesSection = {
+    title: ATTRIBUTES,
+    attributes: [
+        {
+            attributeName: 'Other Names',
+            attributeText: 'Rus (Reggie)'
+        },
+        {
+            attributeName: 'Gender',
+            attributeText: 'Male'
+        },
+        {
+            attributeName: 'Age',
+            attributeText: '13'
+        },
+        {
+            attributeName: 'Height',
+            attributeText: `4' 10"`
+        },
+        {
+            attributeName: 'Birthday',
+            attributeText: 'July 13th, 1983'
+        },
+        {
+            attributeName: 'Birthplace',
+            attributeText: 'Philadelphia, PA'
+        },
+        {
+            attributeName: 'Meta-Human Type',
+            attributeText: 'Mythical'
+        },
+        {
+            attributeName: 'Race',
+            attributeText: 'Human'
+        },
+        {
+            attributeName: 'Hair Color',
+            attributeText: 'Black'
+        },
+        {
+            attributeName: 'Debut',
+            attributeText: 'Chapter 1: Elements of Surprise'
+        },
+    ]
 }
 
 const appearance: PageSection = {
     title: APPEARANCE,
-    body: 'Russel has an afro.'
+    body: (
+        '<strong>Age 13</strong>\n' +
+        'Russel is a skinny dark skinned boy with an unkempt afro. His hair is deceptively long (as seen when Achilles pulls him up by his hair and he is the same height as Reggie off the ground).' +
+        '\n\n' +
+        '<strong>Post Divine Rope</strong>\n' +
+        'After having a piece of mythical rope attached to his back, he grows a pair of orange wings. When tucked, the edges of his wings reach from a few inches above his hair to just below his knees. When expanded they more than cover his hands. On his back is a thin rope shaped like an "X" with the ends reaching both sides of his wings (at about halfway) and the backs of his hands before appearing embedded in his body. He wears shirts with the holes cut out in the back for his wings before being given specialised upper wear.'
+    )
 }
 
-const abilities: PageSection = {
-    title: ABILITIES,
-    body: 'Russel has wings.'
+const abilities: GallerySection = {
+    title: GALLERY,
+    gallery: [
+        {
+            url: DRIVE_THUMBNAIL_PREFIX + '1RJCVJDJ-5EKELmLvd81HGSm-47M0J9iR'
+        }
+    ]
 }
 
 const RusselKing: Page = {
     name: "Russel King",
+    pageImage: {
+        url: DRIVE_THUMBNAIL_PREFIX + '1QAiWVNXwS6Zgvyqxkz2LwEKe1ti3bJ66',
+        caption: 'Russel (Age 13)'
+    },
     sections: [
         headline,
+        attribues,
         appearance,
         abilities,
     ]
